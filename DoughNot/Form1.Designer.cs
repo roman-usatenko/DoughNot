158c56771c9868f63c078826a7e2f9d7699edc23
Roman Usatenko <roman@usatenko.net>
2019-11-24 16:22:18 -0800

    Initial commit

---
+++ DoughNot/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace DoughNot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            ReleaseData();
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.labelLastDetectionTime = new System.Windows.Forms.ToolStripStatusLabel();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.buttonStart = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.textBoxTimeout = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.textBoxDetection = new System.Windows.Forms.ToolStripComboBox();
+            this.captureImageBox = new Emgu.CV.UI.ImageBox();
+            this.labelProgress = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.captureImageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(40, 40);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.labelProgress,
+            this.labelLastDetectionTime});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 1292);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1643, 46);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // labelLastDetectionTime
+            // 
+            this.labelLastDetectionTime.Name = "labelLastDetectionTime";
+            this.labelLastDetectionTime.Size = new System.Drawing.Size(327, 41);
+            this.labelLastDetectionTime.Text = "labelLastDetectionTime";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(40, 40);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.buttonStart,
+            this.toolStripSeparator1,
+            this.toolStripLabel1,
+            this.textBoxTimeout,
+            this.toolStripSeparator2,
+            this.toolStripLabel2,
+            this.textBoxDetection});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1643, 49);
+            this.toolStrip1.TabIndex = 10;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.buttonStart.Image = ((System.Drawing.Image)(resources.GetObject("buttonStart.Image")));
+            this.buttonStart.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(82, 46);
+            this.buttonStart.Text = "Start";
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 49);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(187, 46);
+            this.toolStripLabel1.Text = "Lock in (sec):";
+            // 
+            // textBoxTimeout
+            // 
+            this.textBoxTimeout.Name = "textBoxTimeout";
+            this.textBoxTimeout.Size = new System.Drawing.Size(100, 49);
+            this.textBoxTimeout.ToolTipText = "Integer value between 10 and 600";
+            this.textBoxTimeout.Leave += new System.EventHandler(this.textBoxTimeout_Leave);
+            this.textBoxTimeout.TextChanged += new System.EventHandler(this.textBoxTimeout_TextChanged);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 49);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(154, 46);
+            this.toolStripLabel2.Text = "Detection:";
+            // 
+            // textBoxDetection
+            // 
+            this.textBoxDetection.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.textBoxDetection.Items.AddRange(new object[] {
+            "Default",
+            "Alt",
+            "Alt2",
+            "Alt_Tree"});
+            this.textBoxDetection.Name = "textBoxDetection";
+            this.textBoxDetection.Size = new System.Drawing.Size(121, 49);
+            this.textBoxDetection.SelectedIndexChanged += new System.EventHandler(this.textBoxDetection_SelectedIndexChanged);
+            // 
+            // captureImageBox
+            // 
+            this.captureImageBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.captureImageBox.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
+            this.captureImageBox.Location = new System.Drawing.Point(0, 49);
+            this.captureImageBox.Name = "captureImageBox";
+            this.captureImageBox.Size = new System.Drawing.Size(1643, 1243);
+            this.captureImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.captureImageBox.TabIndex = 2;
+            this.captureImageBox.TabStop = false;
+            // 
+            // labelProgress
+            // 
+            this.labelProgress.Font = new System.Drawing.Font("Courier New", 9F);
+            this.labelProgress.Name = "labelProgress";
+            this.labelProgress.Size = new System.Drawing.Size(33, 41);
+            this.labelProgress.Text = " ";
+            this.labelProgress.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1643, 1338);
+            this.Controls.Add(this.captureImageBox);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DoughNot (Leave Your Desktop Unlocked)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.captureImageBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel labelLastDetectionTime;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton buttonStart;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox textBoxTimeout;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripComboBox textBoxDetection;
+        private Emgu.CV.UI.ImageBox captureImageBox;
+        private System.Windows.Forms.ToolStripStatusLabel labelProgress;
+    }
+}
+
---
+++ DoughNot/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using Emgu.CV;
+using System.Windows.Forms;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace DoughNot
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture _capture;
+        private Mat _frame = new Mat();
+        private DateTime _lastRecognitionTime;
+        private CascadeClassifier _faceClassifier;
+        private bool _progress = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CvInvoke.UseOpenCL = false;
+            try
+            {
+                _capture = new VideoCapture();
+                _capture.ImageGrabbed += ProcessFrame;
+            }
+            catch (Exception excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+            labelLastDetectionTime.Text = "Capture is paused";
+            SystemEvents.SessionSwitch += SystemEvents_SessionSwitch;
+            notifyIcon1.Text = Text;
+        }
+
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            int lastDetectionAgo = (int)(DateTime.Now - _lastRecognitionTime).TotalSeconds;
+            _progress = lastDetectionAgo > 0 && lastDetectionAgo % 5 == 0;
+            if (!_progress)
+            {
+                return;
+            }
+
+            if (_capture != null && _capture.Ptr != IntPtr.Zero)
+            {
+                _capture.Retrieve(_frame, 0);
+                Detect();
+                captureImageBox.Image = _frame;
+            }
+        }
+
+        private void ReleaseData()
+        {
+            SystemEvents.SessionSwitch -= SystemEvents_SessionSwitch;
+            _capture?.Dispose();
+            _faceClassifier?.Dispose();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (_capture != null)
+            {
+                if (timer1.Enabled)
+                {
+                    PauseCapture();
+                }
+                else
+                {
+                    StartCapture();
+                }
+            }
+        }
+
+        private void Detect()
+        {
+            using (InputArray iaImage = _frame.GetInputArray())
+            {
+                using (UMat ugray = new UMat())
+                {
+                    CvInvoke.CvtColor(_frame, ugray, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+                    CvInvoke.EqualizeHist(ugray, ugray);
+                    Rectangle[] facesDetected = _faceClassifier.DetectMultiScale(
+                       ugray,
+                       1.1,
+                       10,
+                       new Size(20, 20));
+
+                    if (facesDetected?.Length > 0)
+                    {
+                        CvInvoke.Rectangle(_frame, facesDetected[0], new Bgr(Color.Red).MCvScalar, 2);
+                        _lastRecognitionTime = DateTime.Now;
+                    }
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan timeSpan = DateTime.Now - _lastRecognitionTime;
+            labelLastDetectionTime.Text = "Face detected " + Math.Round(timeSpan.TotalSeconds) + " seconds ago";
+
+            labelProgress.Text = _progress ? "*" : " ";
+
+            if (timeSpan.TotalSeconds >= Properties.Settings.Default.DetectionTimeout)
+            {
+                LockWorkStation();
+            }
+        }
+
+        [DllImport("user32")]
+        public static extern void LockWorkStation();
+
+        private void PauseCapture()
+        {
+            buttonStart.Text = "Resume";
+            labelLastDetectionTime.Text = "Capture is paused";
+            _capture.Pause();
+            timer1.Enabled = false;
+            _progress = false;
+        }
+
+        private void StartCapture()
+        {
+            _lastRecognitionTime = DateTime.Now;
+            buttonStart.Text = "Pause";
+            labelLastDetectionTime.Text = "Detecting face...";
+            _capture.Start();
+            timer1.Enabled = true;
+        }
+
+        public void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
+        {
+            if (e.Reason == SessionSwitchReason.SessionLock)
+            {
+                PauseCapture();
+            }
+            else if (e.Reason == SessionSwitchReason.SessionUnlock)
+            {
+                StartCapture();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBoxTimeout.Text = "" + Properties.Settings.Default.DetectionTimeout;
+            textBoxDetection.SelectedItem = Properties.Settings.Default.DetectionOption;
+            _faceClassifier = new CascadeClassifier(Application.StartupPath + "\\haarcascade_frontalface_" + textBoxDetection.SelectedItem.ToString().ToLower() + ".xml");
+            StartCapture();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        private bool IsTimeoutValid(out int result)
+        {
+            return int.TryParse(textBoxTimeout.Text, out result)
+                && result >= 10
+                && result <= 600;
+        }
+
+        private void textBoxTimeout_TextChanged(object sender, EventArgs e)
+        {
+            if (!IsTimeoutValid(out int result))
+            {
+                textBoxTimeout.BackColor = Color.LightPink;
+            }
+            else
+            {
+                textBoxTimeout.BackColor = SystemColors.Window;
+                Properties.Settings.Default.DetectionTimeout = result;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void textBoxDetection_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                CascadeClassifier newFaceClassifier = new CascadeClassifier(Application.StartupPath + "\\haarcascade_frontalface_" + textBoxDetection.SelectedItem.ToString().ToLower() + ".xml");
+                _faceClassifier?.Dispose();
+                _faceClassifier = newFaceClassifier;
+                Properties.Settings.Default.DetectionOption = textBoxDetection.SelectedItem.ToString();
+                Properties.Settings.Default.Save();
+            }
+            catch (Exception excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+        }
+
+        private void textBoxTimeout_Leave(object sender, EventArgs e)
+        {
+            if (!IsTimeoutValid(out int result))
+            {
+                textBoxTimeout.Text = "" + Properties.Settings.Default.DetectionTimeout;
+            }
+        }
+    }
+}
